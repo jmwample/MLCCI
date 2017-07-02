09475f867bd78648f6af2da13e6976a054251a9c
Jack W <jackmwampler@gmail.com>
2017-07-02 15:23:20 -0700

    added tweet info extraction and Processing test

--- twitter/test.py
+++ twitter/test.py
@@ -1,11 +1,12 @@
 #!/bin/python 
 """
 
-
 """
 import config as cfg
+import io
 import json
 import re
+from textblob import TextBlob
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -22,17 +23,49 @@ consumer_secret = cfg.api['api_consumer_secret']
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
-        def on_data(self, data):
-            #print data
-            try:
-                tweet = json.loads(data)
-                print(tweet['text'], [ x['text'] for x in tweet['entities']['hashtags'] ])
-            except:
-                return False
+
+    def on_status(self, status):
+        #if status.retweeted_status:
+        #    return
+        description = status.user.description
+        loc = status.user.location
+        text = status.text
+        coords = status.coordinates
+        name = status.user.screen_name
+        user_created = status.user.created_at
+        followers = status.user.followers_count
+        id_str = status.id_str
+        created = status.created_at
+        retweets = status.retweet_count
+        bg_color = status.user.profile_background_color
+
+        print(text)
+
+        blob = TextBlob(text)
+        sentiment = blob.sentiment 
+
+        print (sentiment.polarity, sentiment.subjectivity)
+    
+
+    """def on_data(self, data):
+        #print data
+        try:
+            tweet = json.loads(data)
+            print(tweet['text'], [ x['text'] for x in tweet['entities']['hashtags'] ])
+        except:
+            return False
+        return True
+        """
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+        else:
+            print status
             return True
 
-        def on_error(self, status):
-            print status
+
+
 
 
 if __name__ == '__main__':
@@ -45,3 +78,22 @@ if __name__ == '__main__':
                                                                     
     #This line filter Twitter Streams to capture data by keywords relevant to cryptocurrencies
     stream.filter(track=['bitcoin', 'ethereum', 'btc-usd'])
+
+
+
+
+"""
+mport io
+
+output = io.StringIO()
+output.write('First line.\n')
+print('Second line.', file=output)
+
+# Retrieve file contents -- this will be
+# 'First line.\nSecond line.\n'
+contents = output.getvalue()
+
+# Close object and discard memory buffer --
+# .getvalue() will now raise an exception.
+output.close()
+"""
